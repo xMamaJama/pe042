f98d373da169ecf81ce866e1f69db37d8544b91b
xMamaJama <71728259+xMamaJama@users.noreply.github.com>
2020-11-19 10:42:15 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:nuppu1771@localhost:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_BLACKLIST_ENABLED = True
+    JWT_BLACKLIST_TOKEN_CHECK = ['access', 'refresh']
 
     SECRET_KEY='super-secret-key'
     JWT_ERROR_MESSAGE_KEY = 'message'
